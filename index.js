2bdbce5ac42637c9457d886267b9fdc995ce604f
Orangekun <amazingritro66@gmail.com>
2024-02-27 13:56:59 +0200

    Added events ands 2 commands

--- index.js
+++ index.js
@@ -1,7 +1,8 @@
-const { Client, Events, GatewayIntentBits } = require('discord.js');
+const { Client, Collection, Events, GatewayIntentBits: { Guilds }, Activity, ActivityType } = require('discord.js');
 const { token } = require('./config.json');
+const commands = require('./deploy-commands.js');
 
-const bot = new Client({ intents: [GatewayIntentBits.Guilds] });
+const bot = new Client({ intents: [Guilds] });
 
 const start = async function() {
 	try {
@@ -11,8 +12,36 @@ const start = async function() {
 	}
 };
 
+bot.commands = new Collection(commands);
+bot.ready = require('./events/clientReady');
+bot.interactionCreate = require('./events/interactionCreate');
+
 bot.once(Events.ClientReady, readyClient => {
-	console.log(`${readyClient.user.tag}`);
+	try {
+		bot.ready.execute(bot);
+	} catch (e) {
+		console.error(e);
+	} finally {
+		console.log(`${readyClient.user.tag}`);
+	}
+});
+
+bot.on(Events.InteractionCreate, async interaction => {
+	await bot.interactionCreate.execute(interaction);
+});
+bot.on(Events.GuildMemberAdd, async member => {
+	try {
+		await member.guild.systemChannel.send(`Welcome ${member.displayName} to the ${member.guild.name} server!`);
+	} catch (e) {
+		console.error(e);
+	}
+});
+bot.on(Events.GuildMemberRemove, async member => {
+	try {
+		await member.guild.systemChannel.send(`${member.displayName} has sadly left us!`);
+	} catch (e) {
+		console.error(e);
+	}
 });
 
 start();
